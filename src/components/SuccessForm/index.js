6d33c388afd8317e813ee17cf9bec2bc381dbb2a
Gleb Scherban <posijuur@mail.ru>
2018-02-04 22:25:13 +0200

    ''

---
+++ src/components/SuccessForm/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const style = {
+	title: {
+		fontSize: '18px',
+		fontWeight: '600',
+		color: '#07c0ec',
+	}
+}
+
+
+export default function SuccessForm(props) {
+	return (
+		<div style={style.title}>
+			Congratulations!
+		</div>
+	)
+}
